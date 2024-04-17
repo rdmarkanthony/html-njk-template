b2e0ea1df8dd52e96b30f8df8d960b88181ad4bb
markanthonyrecto <markanthonyrecto@outlook.com>
2024-04-17 22:50:26 +0800

    using tailwind as new css framework with daisyui and theme wireframe

--- gulpfile.js
+++ gulpfile.js
@@ -1,169 +1,178 @@
 const gulp = require("gulp");
-const sass = require('gulp-sass')(require('node-sass'));
-const header = require('gulp-header');
-const htmlbeautify = require('gulp-html-beautify');
-const argv = require('yargs').argv;
+const sass = require("gulp-sass")(require("node-sass"));
+const header = require("gulp-header");
+const htmlbeautify = require("gulp-html-beautify");
+const argv = require("yargs").argv;
+const postcss = require("gulp-postcss");
+const autoprefixer = require("autoprefixer");
+const tailwindcss = require("tailwindcss");
 
 // load all required plugins (listed in package.json)
 const plugins = require("gulp-load-plugins")({
-    pattern: "*"
+  pattern: "*",
 });
 
 console.log(plugins); // logs loaded plugins in terminal
 
 const reload = (after) => {
-    plugins.browserSync.reload();
-    after();
-}
+  plugins.browserSync.reload();
+  after();
+};
 
 // loads browsersync
 gulp.task("browser-sync", (after) => {
-    plugins.browserSync.init({
-        server: {
-            baseDir: "./public",
-            middleware: function (req, res, next) {
-                res.setHeader('Access-Control-Allow-Origin', '*');
-                next();
-            }
-        }
-    });
-    after();
+  plugins.browserSync.init({
+    server: {
+      baseDir: "./public",
+      middleware: function (req, res, next) {
+        res.setHeader("Access-Control-Allow-Origin", "*");
+        next();
+      },
+    },
+  });
+  after();
 });
 
 // renders nunjucks
 gulp.task("njk", () => {
-    // Gets .html and .njk files in pages
-    return gulp
-        .src("./pages/**/**/*.+(html|njk)")
-        .pipe(
-            plugins.nunjucksRender({
-                path: ["./templates"]
-            })
-        )
-        .on('error', function (err) {
-            console.log(err.message); //don't output error for now
-            console.log(err.fileName); //don't output error for now
-            this.emit('end');
+  // Gets .html and .njk files in pages
+  return (
+    gulp
+      .src("./pages/**/**/*.+(html|njk)")
+      .pipe(
+        plugins.nunjucksRender({
+          path: ["./templates"],
         })
-        .pipe(htmlbeautify({
-            preserve_newlines: false
-        }))
-        // output files in app folder
-        .pipe(gulp.dest("./public"))
+      )
+      .on("error", function (err) {
+        console.log(err.message); //don't output error for now
+        console.log(err.fileName); //don't output error for now
+        this.emit("end");
+      })
+      .pipe(
+        htmlbeautify({
+          preserve_newlines: false,
+        })
+      )
+      // output files in app folder
+      .pipe(gulp.dest("./public"))
+  );
 });
 
 // compile scss
 gulp.task("styles", () => {
-    return gulp
-        .src(["./assets/scss/**/*.scss", "!./assets/scss/fontawesome/**/*.scss"])
-        .pipe(
-            header("$folder: '../fonts';\n"))
-        .pipe(
-            sass({
-                outputStyle: 'compressed'
-            }).on('error', sass.logError))
-        .pipe(
-            plugins.autoprefixer({
-                overrideBrowserslist: ["last 3 versions"],
-                cascade: false
-            })
-        )
-        .pipe(gulp.dest("./public/assets/css/"))
+  return (
+    gulp
+      .src(["./assets/scss/**/*.scss", "!./assets/scss/fontawesome/**/*.scss"])
+      .pipe(header("$folder: '../fonts';\n"))
+      .pipe(
+        sass({
+          outputStyle: "compressed",
+        }).on("error", sass.logError)
+      )
+      // .pipe(
+      //   plugins.autoprefixer({
+      //     overrideBrowserslist: ["last 3 versions"],
+      //     cascade: false,
+      //   })
+      // )
+      .pipe(postcss([tailwindcss, autoprefixer]))
+      .pipe(gulp.dest("./public/assets/css/"))
+  );
 });
 
 // compile js
 gulp.task("scripts", () => {
-    return gulp
-        .src([
-            // libraries
-            "./assets/js/lib/jquery.min.js",
-            "./assets/js/lib/jquery.inview.min.js",
+  return gulp
+    .src([
+      // libraries
+      "./assets/js/lib/jquery.min.js",
+      "./assets/js/lib/jquery.inview.min.js",
 
-            // custom
-            "./assets/js/custom.js"
-        ])
-        .pipe(plugins.concat("custom.js"))
-        .pipe(gulp.dest("./public/assets/js/"));
+      // custom
+      "./assets/js/custom.js",
+    ])
+    .pipe(plugins.concat("custom.js"))
+    .pipe(gulp.dest("./public/assets/js/"));
 });
 
 // linters
 gulp.task("lint-styles", () => {
-    return gulp
-        .src(["./assets/scss/**/*.scss", "!assets/scss/vendor/**/*.scss"])
-        .pipe(plugins.sassLint())
-        .pipe(plugins.sassLint.format())
-        .pipe(plugins.sassLint.failOnError())
+  return gulp
+    .src(["./assets/scss/**/*.scss", "!assets/scss/vendor/**/*.scss"])
+    .pipe(plugins.sassLint())
+    .pipe(plugins.sassLint.format())
+    .pipe(plugins.sassLint.failOnError());
 });
 
 gulp.task("lint-scripts", () => {
-    return gulp
-        .src(["./assets/js/**/*.js", "!node_modules/**"])
-        .pipe(plugins.eslint())
-        .pipe(plugins.eslint.format())
-        .pipe(plugins.eslint.failAfterError());
+  return gulp
+    .src(["./assets/js/**/*.js", "!node_modules/**"])
+    .pipe(plugins.eslint())
+    .pipe(plugins.eslint.format())
+    .pipe(plugins.eslint.failAfterError());
 });
 
 // merge and minify files
 gulp.task("concat-styles", () => {
-    return gulp
-        .src(["./assets/scss/**/*.scss", "!./assets/scss/fontawesome/**/*.scss"])
-        .pipe(
-            header("$folder: '../fonts';\n")
-        )
-        .pipe(
-            sass({ outputStyle: 'compressed' }).on('error', sass.logError)
-        )
-        .pipe(
-            plugins.autoprefixer({
-                overrideBrowserslist: ["last 3 versions"],
-                cascade: false
-            })
-        )
-        .pipe(gulp.dest("./public/assets/css/"))
-        .pipe(
-            plugins.rename({
-                suffix: ".min"
-            })
-        )
-        // .pipe(plugins.sourcemaps.write())
-        .pipe(gulp.dest("./public/assets/css/"));
+  return (
+    gulp
+      .src(["./assets/scss/**/*.scss", "!./assets/scss/fontawesome/**/*.scss"])
+      .pipe(header("$folder: '../fonts';\n"))
+      .pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError))
+      .pipe(
+        plugins.autoprefixer({
+          overrideBrowserslist: ["last 3 versions"],
+          cascade: false,
+        })
+      )
+      .pipe(gulp.dest("./public/assets/css/"))
+      .pipe(
+        plugins.rename({
+          suffix: ".min",
+        })
+      )
+      // .pipe(plugins.sourcemaps.write())
+      .pipe(gulp.dest("./public/assets/css/"))
+  );
 });
 
 gulp.task("concat-scripts", () => {
-    return gulp
-        .src(["./public/assets/js/custom.js"])
-        .pipe(plugins.concat("custom.js"))
-        .pipe(plugins.uglify())
-        .on('error', function (err) {
-            console.log(err)
+  return (
+    gulp
+      .src(["./public/assets/js/custom.js"])
+      .pipe(plugins.concat("custom.js"))
+      .pipe(plugins.uglify())
+      .on("error", function (err) {
+        console.log(err);
+      })
+      .pipe(
+        plugins.rename({
+          suffix: ".min",
         })
-        .pipe(
-            plugins.rename({
-                suffix: ".min"
-            })
-        )
-        // .pipe(plugins.sourcemaps.write())
-        .pipe(gulp.dest("./public/assets/js"));
+      )
+      // .pipe(plugins.sourcemaps.write())
+      .pipe(gulp.dest("./public/assets/js"))
+  );
 });
 
 // gulp watch
-gulp.task("watch", gulp.series("njk", "styles", "scripts", "browser-sync", () => {
+gulp.task(
+  "watch",
+  gulp.series("njk", "styles", "scripts", "browser-sync", () => {
     // watch njk files
-    gulp.watch([
-        "./pages/**/*.+(html|njk)",
-        "./templates/**/*.+(html|njk)",
-    ], gulp.series("njk", reload));
+    gulp.watch(
+      ["./pages/**/*.+(html|njk)", "./templates/**/*.+(html|njk)"],
+      gulp.series("njk", "styles", reload)
+    );
 
     // watch sass files
-    gulp.watch([
-        "./assets/scss/**/*.scss",
-    ], gulp.series("styles", reload));
+    gulp.watch(["./assets/scss/**/*.scss"], gulp.series("styles", reload));
 
     // watch js files
-    gulp.watch([
-        "./assets/js/*.js",
-    ], gulp.series("scripts", reload));
-}));    
+    gulp.watch(["./assets/js/*.js"], gulp.series("scripts", "styles", reload));
+  })
+);
 
 gulp.task("lint", gulp.series("lint-styles", "lint-scripts")); // lint css + js files
 gulp.task("merge", gulp.series("concat-styles", "concat-scripts")); // merge & minify css + js
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{html,njk}",
+    "./templates/**/*.{html,njk}",
+    "./assets/scss/**/*.{scss}",
+    "./assets/js/**/*.{js}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require("tailwindcss"),
+    require("autoprefixer"),
+    require("daisyui"),
+  ],
+  daisyui: {
+    themes: ["wireframe"],
+  },
+};
