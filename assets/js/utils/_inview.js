2bb90854bbabab6a9d6f204c4a19501582c9e875
Mark Anthony Recto <113584729+rdmarkanthony@users.noreply.github.com>
2025-02-23 19:08:47 +0800

    update: class _inview

--- assets/js/utils/_inview.js
+++ assets/js/utils/_inview.js
@@ -1,11 +1,38 @@
 class _inview {
     constructor(props) {
         this.target = props.target ?? null;
+        this.observeOnce = props.observeOnce !== false;
+        this.observeRatio = props.observeRatio ?? 0;
+        this.threshold = props.threshold ?? 0;
 
-        if (this.target) this.init();
+        this.visible = false;
+
+        if (props.target) this.init(props.callback);
     }
 
-    init() {
-        console.log(this);
+    init(callback) {
+        const _observer = new IntersectionObserver(
+            (entries) => {
+                entries.forEach((entry) => {
+                    if (entry.isIntersecting && entry.intersectionRatio >= this.observeRatio) {
+                        if (this.visible) return;
+                        this.visible = true;
+
+                        if (callback) callback();
+
+                        if (this.observeOnce) _observer.disconnect();
+                    } else {
+                        this.visible = false;
+                    }
+                });
+            },
+            {
+                root: null,
+                rootMargin: "0px",
+                threshold: this.threshold,
+            }
+        );
+
+        _observer.observe(this.target);
     }
 }
